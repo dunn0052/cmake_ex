01e60d68614fb69a0f3d5fc0e69907e049d14cfc
Kevin Dunn <dunn0052@gmail.com>
2022-05-20 07:01:33 -0500

    Initial git commit

---
+++ CMakeFiles/readme.txt
@@ -0,0 +1,11 @@
+# Release
+cd ..
+mkdir release
+cd release
+cmake -DCMAKE_BUILD_TYPE=Release ../CMakeFiles
+
+# Debug
+cd ..
+mkdir debug
+cd debug
+cmake -DCMAKE_BUILD_TYPE=Debug ../CMakeFiles
---
+++ debug/CMakeFiles/CMAKE_EX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMAKE_EX"
+  "CMAKE_EX.pdb"
+  "CMakeFiles/CMAKE_EX.dir/home/pi/Desktop/Projects/cmake_ex/project/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMAKE_EX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
